64d3b71f7403ee616d84beb52de53de8ce494751
harikrishnan <kphari891@gmail.com>
2022-11-23 16:03:15 +0530

    Initial commit

---
+++ bin/untitled10.dart
@@ -0,0 +1,15 @@
+void main(){
+  var username = "abu";
+  var password = 2200;
+  print(username == 'abu' && password == 2200);
+  print(username == 'abu' && password == 200);
+  print(username == 'bu' && password == 2200);
+  print(username == 'abu' || password == 2200);
+  print(username == 'bu' || password == 2200);
+  print(username == 'abu' || password == 200);
+  print(!(username == 'abu' && password == 2200));
+
+
+
+
+}
---
+++ test/untitled10_test.dart
@@ -0,0 +1,8 @@
+import 'package:untitled10/untitled10.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
